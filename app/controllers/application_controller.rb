fa21d287a02a2517a8b82fd0dca126913d982e34
ghesketh76 <ghesketh76@gmail.com>
2021-03-18 13:19:10 -0600

    users resource with auth

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,36 @@
+class ApplicationController < ActionController::API
+
+
+    def current_user
+        auth_header = request.headers['Authorization']
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                secret = ENV['SECRET_KEY_BASE']
+                user_id = JWT.decode(token, secret)[0]["user_id"]
+                @user = User.find(user_id)
+            rescue
+                render json: {error: "Bad Token"}, status: :unauthorized
+            end
+        end
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def authorized 
+        render json: {message: "Please Log In"}, status: :unauthorized unless logged_in?
+    end
+
+    def login 
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            secret = ENV['SECRET_KEY_BASE']
+            @token = JWT.encode({user_id: @user.id}, secret)
+            render json: {user: @user, token: @token}, status: :accepted
+        else
+            render json: {errors: ['Invalid username or password']}, status: :unauthorized
+        end
+    end
+end
