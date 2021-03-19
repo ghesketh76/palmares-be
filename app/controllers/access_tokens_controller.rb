04100220ca9efd90bfb2efc0067380e8a5e34d8e
ghesketh76 <ghesketh76@gmail.com>
2021-03-18 22:50:35 -0600

    finish access token controller

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,2 +1,23 @@
 class AccessTokensController < ApplicationController
+
+    def show 
+        @access_token = AccessToken.all 
+        render json: @access_token
+    end
+
+    def create 
+        @access_token = AccessToken.create(
+                            user_id: params[:user_id], 
+                            scope: params[:scope], 
+                            access_token: params[:access_token], 
+                            expires_at: params[:expires_at]
+                            )
+        render json: @access_token, status: :created
+    end
+
+    def update 
+        @access_token = AccessToken.find(params[:user_id])
+        @access_token.update(access_token: params[:access_token], expires_at: params[:expires_at])
+        render json: @access_token, status: :reset_content
+    end
 end
