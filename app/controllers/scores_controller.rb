04ed800b3a60f5acfe88d6eb5aa943438d5a36e5
ghesketh76 <ghesketh76@gmail.com>
2021-03-26 16:02:55 -0600

    added score resource

---
+++ app/controllers/scores_controller.rb
@@ -0,0 +1,11 @@
+class ScoresController < ApplicationController
+
+    def index 
+        @score = Score.all 
+        render json: @score, includes: :user
+    end
+
+    def create 
+        @score = Score.create(user_id: params[:user_id], score: params[:score])
+    end
+end
